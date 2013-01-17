4d14e0d2b8413086a6ff210f3692154ebb8ad2e2
flo076 <florian.jose.ferreira@gmail.com>
2013-01-18 00:55:29 +0100

    New question = send mail

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,12 +1,15 @@
 package controllers;
 
 import play.*;
+import play.libs.Mail;
 import play.mvc.*;
 
 import groovyjarjarantlr.StringUtils;
 
 import java.util.*;
 
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
 import org.codehaus.groovy.util.StringUtil;
 
 import models.*;
@@ -25,14 +28,38 @@ public class Application extends Controller {
 	 * @param question
 	 *            the question
 	 * @return the string
+	 * @throws EmailException
 	 */
-	public static String index(String q) {
+	public static String index(String q) throws EmailException {
+		String answer = "No question to treated";
+		if (org.apache.commons.lang.StringUtils.isNotBlank(q)) {
+			answer = treatedQuestion(q);
+
+		}
+		return answer;
+	}
+
+	private static String treatedQuestion(String q) throws EmailException {
+		String answer;
 		Logger.info("new question: \n" + q + "\n");
+		sendMail(q);
 		if (EMAIL.equalsIgnoreCase(q)) {
 			Logger.info("email question");
-			return "florian.jose.ferreira@gmail.com";
+			answer = "florian.jose.ferreira@gmail.com";
+		} else {
+			Logger.info("question not treated");
+			answer = "Question not treated";
 		}
-		Logger.info("question not treated");
-		return "Not treated";
+		return answer;
+	}
+
+	private static void sendMail(String q) throws EmailException {
+		SimpleEmail email = new SimpleEmail();
+		email.setFrom("florian.jose.ferreira@gmail.com");
+		email.addTo("florian.jose.ferreira@gmail.com");
+		email.setSubject("New question");
+		email.setMsg(q);
+		email.getHostName();
+		Mail.send(email);
 	}
 }
