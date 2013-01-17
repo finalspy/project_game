0e82919fde37a62eaa2646f13f88a9d9b8ea56f6
flo076 <florian.jose.ferreira@gmail.com>
2013-01-18 00:28:09 +0100

    First question

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,14 +3,32 @@ package controllers;
 import play.*;
 import play.mvc.*;
 
+import groovyjarjarantlr.StringUtils;
+
 import java.util.*;
+
+import org.codehaus.groovy.util.StringUtil;
 
 import models.*;
 
+/**
+ * The Class Application.
+ */
 public class Application extends Controller {
 
-    public static String index(String p) {
-    	return "test";
-    }
+	/** The Constant EMAIL. */
+	private static final String EMAIL = "Quelle est ton adresse email";
 
+	/**
+	 * Index.
+	 *
+	 * @param question the question
+	 * @return the string
+	 */
+	public static String index(String q) {
+		if (EMAIL.equalsIgnoreCase(q)) {
+			return "florian.jose.ferreira@gmail.com";
+		}
+		return "Not treated";
+	}
 }
